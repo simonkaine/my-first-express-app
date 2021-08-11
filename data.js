84c81e96b87c319dfa0337524fbdd5fe13368add
Simon Kaine <simonkaine@gmail.com>
2021-08-10 17:38:45 -0700

    Added boolean and dropdown condition to data module.

--- data.js
+++ data.js
@@ -1,12 +1,34 @@
 const data = [
     {
         id:1,
-        name: 'Simon'
+        name: 'Simon Kaine',
+        bad: false,
+        species: 'human',
     },
     {
         id:2,
-        name: 'Kaine'
-    }
+        name: 'Glerg',
+        bad: true,
+        species: 'plutonian',
+    },
+    {
+        id:3,
+        name: 'Noob Noob',
+        bad: true,
+        species: 'human',
+    },
+    {
+        id:4,
+        name: 'Flim-flom',
+        bad: false,
+        species: 'plutonian',
+    },
+    {
+        id:5,
+        name: 'Flim-flom',
+        bad: true,
+        species: 'martian',
+    },
 ]
 
 module.exports = data;
